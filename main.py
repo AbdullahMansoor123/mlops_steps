805bf8cef5dba986cfb76d50aa4297217c6d1c2c
AbdullahMansoor123 <abdullahmansoor123@gmail.com>
2025-04-14 19:31:28 +0500

    added wandb and hydra config

--- main.py
+++ main.py
@@ -6,44 +6,54 @@ from pipelines.model import SimpleCNN
 from pipelines.train import Trainer
 
 import wandb
+import omegaconf
 
-def main():
+import hydra
+from omegaconf.omegaconf import OmegaConf
+
+
+@hydra.main(config_path="./configs", config_name="config")
+
+def main(cfg):
     """
     Main function to execute the entire workflow.
     """
+
+
+
     config = {
-        "batch" : 32,
-        "epochs" : 5,
-        "val_split" : 0.2,
-        "test_split" : 0.2,
-        "num_classes" : 10,
-        "lr" : 3e-5,
-        "loss": "CrossEntropyLoss",
-        "optimizer": "AdamW",
-        "model": SimpleCNN,
-        "wandb_project": "mlops-steps"
+            "batch" : 32,
+            "epochs" : 3,
+            "val_split" : 0.2,
+            "test_split" : 0.2,
+            "num_classes" : 10,
+            "lr" : 3e-5,
+            "loss": "CrossEntropyLoss",
+            "optimizer": "AdamW",
+            "model": SimpleCNN,
+            "wandb_project": "mlops-steps"
     }
+    # print(OmegaConf.to_yaml(cfg))
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    data_module = MNISTDataModule(batch_size=config["batch"], 
-                                  val_split= config["val_split"], 
-                                  test_split=config["test_split"])
+    data_module = MNISTDataModule(batch_size= cfg.processing.batch_size, 
+                                  val_split= cfg.processing.val_split, 
+                                  test_split= cfg.processing.test_split)
     data_module.setup()
     train_loader, val_loader = data_module.train_loader, data_module.val_loader
-
     
     # Load pre-trained model
-    model = SimpleCNN(config["num_classes"]).to(device)
+    model = SimpleCNN(cfg.model.num_classes).to(device)
     loss = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=config["lr"])
-    
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.training.lr)
 
-    # Initialize wandb
+    # # Initialize wandb
+    wandb.config = omegaconf.OmegaConf.to_container(
+        cfg, resolve=True, throw_on_missing=True
+    )
     exp_name = f"mnist_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    wandb.init(project= config["wandb_project"], 
-                name= exp_name,
-                config= config)
-
+    wandb.init(project= cfg.training.wandb_project, 
+                name= exp_name)
 
     # Train the model
     training = Trainer(
@@ -52,7 +62,7 @@ def main():
                         val_loader,
                         loss,
                         optimizer,
-                        epochs=config["epochs"]
+                        epochs=cfg.training.epochs
                     
                     )
     
--- pipelines/train.py
+++ pipelines/train.py
@@ -2,12 +2,15 @@ import os
 
 import torch
 import torch.nn as nn
+import wandb.sklearn
 from pipelines.data import MNISTDataModule
 from pipelines.model import SimpleCNN
 from torch import optim
 # import the library
 import wandb
 
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -34,6 +37,8 @@ class Trainer:
         for epoch in range(self.epochs):
             self.model.train()
             running_loss = 0.0
+            correct = 0
+            total = 0
             for images, labels in self.train_loader:
                 images, labels = images.to(device), labels.to(device)
                 self.optimizer.zero_grad()
@@ -41,28 +46,62 @@ class Trainer:
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
+
                 running_loss += loss.item()
+                # print(f"len outputs {len(outputs)} outputs{outputs}")
+                _, predicted = torch.max(outputs, 1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
+            
+            train_loss = running_loss / len(self.train_loader)
+            train_acc = 100 * correct / total
+            
+            wandb.log({"epoch": epoch + 1, "train_loss": train_loss, "train_accuracy": train_acc})
 
-            avg_loss = running_loss / len(self.train_loader)
-            print(f"Epoch {epoch+1}, Loss: {avg_loss:.4f}")
-            wandb.log({"epoch": epoch + 1, "train_loss": avg_loss})
-
-            self.validate(epoch+1)
+            self.validate(epoch, train_acc, train_loss)
             
             torch.save(self.model.state_dict(), "mnist_cnn.pth")
 
-    def validate(self,epoch):
+    def validate(self,epoch, train_loss,train_acc):
         self.model.eval()
+        running_loss = 0.0
         correct = 0
         total = 0
+
+        all_preds = []
+        all_labels = []
         with torch.no_grad():
             for images, labels in self.val_loader:
                 images, labels = images.to(device), labels.to(device)
                 outputs = self.model(images)
+                loss = self.criterion(outputs, labels)
+                running_loss += loss.item()
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+
+                all_preds.extend(predicted.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+
+        val_loss = running_loss / len(self.train_loader)
+        val_acc = 100 * correct / total
+        print(f"Epoch {epoch+1},  Train_Loss: {train_loss:.4f} | Train_Accuracy: {train_acc:.2f}% | Val_Loss: {val_loss:.4f} | Val_Accuracy: {val_acc:.2f}%")
+        wandb.log({"epoch": epoch + 1, "al_loss": val_loss, "val_accuracy": val_acc})
+
+        precision = precision_score(y_true=all_labels, y_pred=all_preds, average='macro') 
+        recall = recall_score(y_true=all_labels, y_pred=all_preds, average='macro')
+        f1 = f1_score(y_true=all_labels, y_pred=all_preds, average='macro')
+        print(f"Precision: {precision:.2f} | Recall: {recall:.2f} | F1: {f1:.2f} ")
+        wandb.log({"precision": precision})
+        wandb.log({"recall": recall})
+        wandb.log({"f1_score": f1})
         
-        accuracy = 100 * correct / total
-        print(f'Validation Accuracy: {accuracy:.2f}%')
-        wandb.log({"epoch": epoch + 1, "accuracy": accuracy})
+         
+        # 2. Confusion Matrix plotting using scikit-learn method
+        class_names = ['0','1','2','3','4','5','6','7','8','9']
+        wandb.log({"conf_matrix" : wandb.sklearn.plot_confusion_matrix(
+                        y_true=all_labels, y_pred=all_preds,
+                        labels=class_names)})
+
+       
+
